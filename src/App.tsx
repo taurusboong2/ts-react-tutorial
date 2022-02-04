65b95a13df5f0371964181564c4284cb1a20b95d
taurusboong2 <taurusboong2@gmail.com>
2022-02-04 15:40:36 +0900

    Context API with TS

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Counter from './Counter';
 import ReducerSample from './ReducerSample';
+import { SampleProvider } from './SampleContext';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Counter />
-      <ReducerSample />
+      <SampleProvider>
+        <ReducerSample />
+      </SampleProvider>
     </div>
   );
 };
