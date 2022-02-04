8ec54722f66aa4b84e817e1ef050e35c6dbc4e4c
taurusboong2 <taurusboong2@gmail.com>
2022-02-04 13:48:18 +0900

    1st

---
+++ src/Greetings.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+type GreetingsProps = {
+  name: string;
+  mark: string;
+};
+
+const Greetings = ({ name, mark }: GreetingsProps) => {
+  return (
+    <div>
+      Hello, {name}
+      {mark}
+    </div>
+  );
+};
+
+Greetings.defaultProps = {
+  mark: '!!',
+};
+
+export default Greetings;
